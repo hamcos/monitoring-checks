fe67bb92fdb2e52a01fc3446ce514320629b77bb
Robin Schneider <robin.schneider@hamcos.de>
2015-08-20 16:07:13 +0200

    Wrote hh3c_entity_cpu. WIP hh3c_entity_temp.

---
+++ check_mk/hh3c_entity/web/plugins/perfometer/hh3c_entity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# @author Robin Schneider <robin.schneider@hamcos.de>
+# @company hamcos IT Service GmbH http://www.hamcos.de
+# @license AGPLv3 <https://www.gnu.org/licenses/agpl-3.0.html>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, version 3 of the
+# License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+perfometers["check_mk-hh3c_entity_cpu"] = perfometer_cpu_utilization
